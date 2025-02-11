78ecb7131fd43d1cbe462137e38eb3360de1f8f6
Vasanth Kumar <vasanthvc02@gmail.com>
2025-02-11 19:45:44 +0530

    'release'

--- src/App.js
+++ src/App.js
@@ -5,23 +5,17 @@ import Order from "./Components/Order/Order.jsx";
 import Cart from "./Components/Cart/Cart.jsx";
 import Menu from "./Components/Menu/Menu.jsx";
 
-
-
-
-
-
 function App() {
   return (
     <div>
-          <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="menu" element={<Menu />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="order" element={<Order />} />
-    </Routes>
-    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="menu" element={<Menu />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="order" element={<Order />} />
+      </Routes>
+      <Navbar />
     </div>
-
   );
 }
 
